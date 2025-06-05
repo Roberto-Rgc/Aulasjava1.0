289e03411942ea70c102c9cae3b97d93f23a7e0e
Roberto-Rgc <robetodegoescorreia@gmail.com>
2025-06-05 12:25:04 -0300

    gerente

---
+++ src/ClasseGerente.java
@@ -0,0 +1,12 @@
+
+public class ClasseGerente extends Classeindividuo{
+	//Atributo
+	private String setorAt;
+	
+	//Construtor
+	public ClasseGerente(String nome, int renda, String setor) {
+		Super(nome, renda, setor);
+		this.setorAt = setor;
+	}
+  
+}
